8ece6fa7b0ecc5561adb51dc55bd2e12fb09a853
John Doe <nithin.iitkgp@gmail.com>
2024-08-31 09:49:49 -0700

    updated features file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "14019ec5607fa57c"; }
+/******/ 		__webpack_require__.h = function() { return "01c6b36d5b1ec012"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
